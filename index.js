26d8f9f96fba0f018795885d18b88d32b898fba6
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-02 18:46:44 -0500

    adding new API request

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 //Accordion Script(<script src="https://code.jquery.com/jquery-1.12.4.js"></script> <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>)
 
 //API key
-var apiKey = "c4ce918cf9734c35b52566ea7f18c95f"
+var apiKey = "c4ce918cf9734c35b52566ea7f18c95f";
 
 //Fetch
-fetch("https://api.rawg.io/api/platforms?key="+apiKey, {
-	"method": "GET",
+fetch("https://api.rawg.io/api/platforms?key=" + apiKey, {
+  method: "GET",
 })
-.then(response => {
-	console.log(response);
-  return response.json();
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    //Appending Images to tag/id
+    for (var i = 0; i < data.results.length; i++) {
+      console.log(data.results[i].image_background);
+      // var backgroundImage = document
+      // var issueTitle = document
+      // userName.textContent = data[i].user.login;
+      // issueTitle.textContent = data[i].title;
+      // issueContainer.append(userName);
+      // issueContainer.append(issueTitle);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+fetch("https://api.rawg.io/api/games?key=" + apiKey, {
+  method: "GET",
 })
-.then(function (data) {
-  console.log(data)
-  //Appending Images to tag/id
-  for (var i = 0; i < data.results.length; i++) {
-    console.log(data.results[i].image_background)
-    // var backgroundImage = document
-    // var issueTitle = document
-    // userName.textContent = data[i].user.login;
-    // issueTitle.textContent = data[i].title;
-    // issueContainer.append(userName);
-    // issueContainer.append(issueTitle);
-  }
-})
-.catch(err => {
-	console.error(err);
-});
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
 
+    for (var i = 0; i < data.results.length; i++) {
+      console.log(data.results[i].background_image);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 //Pictures
-$( function() {
-    $( "#accordion" ).accordion();
-  } );
+$(function () {
+  $("#accordion").accordion();
+});
