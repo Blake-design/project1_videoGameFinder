b9b28c41461c28af374109997b5075a7730b89ba
Blake McCarty <blake_mccarty@yahoo.com>
2021-09-10 13:42:36 -0500

    code clean up
    this set up objects to clean up the code

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ fetch("https://api.rawg.io/api/games?key=" + apiKey, {
   })
   .then(function (data) {
     console.log(data);
+<<<<<<< Updated upstream
   })
   .catch((err) => {
     console.error(err);
@@ -34,3 +35,44 @@ fetch("https://api.rawg.io/api/games?key=" + apiKey, {
 /// set up click event that stores selction to local storage
 
 ///set local storage to variable let that filter results
+=======
+
+    game1 = {
+      name: data.results[i].name,
+      photo: data.results[0].background_image,
+      genres: data.results[0].genres[0].name,
+      rating: data.results[0].rating_top,
+    };
+
+    game2 = {
+      name: data.results[1].name,
+      photo: data.results[1].background_image,
+      genres: data.results[1].genres[0].name,
+      rating: data.results[1].rating_top,
+    };
+    game3 = {
+      name: data.results[2].name,
+      photo: data.results[2].background_image,
+      genres: data.results[2].genres[0].name,
+      rating: data.results[2].rating_top,
+    };
+    game4 = {
+      name: data.results[3].name,
+      photo: data.results[3].background_image,
+      genres: data.results[3].genres[0].name,
+      rating: data.results[3].rating_top,
+    };
+    game5 = {
+      name: data.results[4].name,
+      photo: data.results[4].background_image,
+      genres: data.results[4].genres[0].name,
+      rating: data.results[4].rating_top,
+    };
+
+    $("#photo1").attr("src", game1.photo);
+    $("#photo2").attr("src", game2.photo);
+    $("#photo3").attr("src", game3.photo);
+    $("#photo4").attr("src", game4.photo);
+    $("#photo5").attr("src", game5.photo);
+  });
+>>>>>>> Stashed changes
