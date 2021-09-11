21e31a3ca6eb1b38af289255c0bd0ff45758c189
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-11 12:12:11 -0500

    new code

--- index.js
+++ index.js
@@ -2,46 +2,9 @@
 
 //API key
 var apiKey = "c4ce918cf9734c35b52566ea7f18c95f";
-// fetch("https://api.rawg.io/api/genres?key=" + apiKey, {
-//   method: "GET",
-// })
-//   .then((response) => {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then(function (genres) {
-//     console.log(genres);
-//     for (var i = 0; i < 5; i++) {
-//       mainGenres = genres.results[i].name;
-//       console.log(mainGenres);
-//       genre.text(mainGenres);
-//     }
-//     console.log(genre);
-//     for (var i = 1; i < genre.length; i++) {
-//       console.log(genre.chartAt(i));
-//     }
-//     $("#button").click(function () {
-//       if (mainGenres) {
-//         for (var i = 0; i < 5; i++) {
-//           showGames = genres.results[i].games;
-//           console.log(showGames);
-//         }
-//       }
-//       fetch(
-//         fetch(`https://api.rawg.io/api/games?key=` + apiKey, {
-//           method: "GET",
-//         })
-//       )
-//         .then((data) => {
-//           console.log(data);
-//           return data.json();
-//         })
-//         .then(function (allData) {
-//           console.log(allData);
-//         });
-// });
 
 //Fetch
+
 fetch("https://api.rawg.io/api/games?key=" + apiKey, {
   method: "GET",
 })
@@ -54,6 +17,10 @@ fetch("https://api.rawg.io/api/games?key=" + apiKey, {
 
     $("article").each(function (index) {
       $(this).children("img").attr("src", data.results[index].background_image);
+      $(this).children("div").text(data.results[index].name);
+    });
+
+    $("aside").each(function (index) {
       $(this).children("div").text(data.results[index].name);
 
       console.log(index);
