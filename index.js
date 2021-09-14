0e64b7917b191b999c219d451192c64a496b656d
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-13 20:31:08 -0500

    added genre filter

--- index.js
+++ index.js
@@ -22,8 +22,12 @@ $(function () {
 });
 
 // call API
-function myFetch() {
-  var genre = $(".genre").val();
+function myFetch(change) {
+  var selectedGenre = $("#genre").val();
+  if (change) {
+    selectedGenre = change;
+  }
+  console.log(selectedGenre);
 
   fetch("https://api.rawg.io/api/games?key=" + apiKey, {
     method: "GET",
@@ -32,51 +36,53 @@ function myFetch() {
       return response.json();
     })
     .then(function (data) {
-      var myArray = [];
+      var videoGameArray = [];
       console.log(data);
-      buildPage(data, genre);
-      console.log(myArray);
 
       data.results.forEach((result) => {
         result.genres.forEach((responseGenre) => {
-          $(".pure-button").click(function updateGames() {
-            if (genre === responseGenre.name) {
-              myArray.push(responseGenre);
-            }
-          });
+          console.log(responseGenre);
+          console.log(selectedGenre);
+          if (selectedGenre === responseGenre.name) {
+            console.log("it worked");
+            videoGameArray.push(result);
+          }
         });
       });
+
+      console.log(videoGameArray);
+      buildPage(videoGameArray);
     });
 }
 
 ////this will build the page
 
 function buildPage(data, genre) {
+  if (!data) {
+    return;
+  }
+
   $("article").each(function (index) {
     $(this)
       .children("div")
       .children("div")
       .children("h4")
-      .text("ESRB Rating: " + data.results[index].esrb_rating.name);
+      .text("ESRB Rating: " + data[index].esrb_rating.name);
     $(this)
       .children("div")
       .children("div")
       .children("h5")
-      .text("Metacritic Rating: " + data.results[index].metacritic);
+      .text("Metacritic Rating: " + data[index].metacritic);
     $(this)
       .children("div")
       .children("div")
       .children("h6")
       .text("Insert data from second API HERE");
-    $(this).children("div").children("h3").text(data.results[index].name);
-    $(this).children("img").attr("src", data.results[index].background_image);
-    $(this).children("aside").children("div").text(data.results[index].name);
+    $(this).children("div").children("h3").text(data[index].name);
+    $(this).children("img").attr("src", data[index].background_image);
+    $(this).children("aside").children("div").text(data[index].name);
   });
 }
-
-$(".genre").change($(".genre").val());
-
-console.log($(".genre").val());
 
 //Game deals
 var requestOptions = {
@@ -86,6 +92,7 @@ var requestOptions = {
 
 //Moduel 6.3
 
+var tableBody = document.getElementById("repo-table");
 var search = $("#searchValue").val();
 function getDeals() {
   fetch(
@@ -118,8 +125,6 @@ function getDeals() {
 $("#fetch-button").click(getDeals);
 
 $(".button").click(myFetch());
-
-$(".genre").change($(".genre").val());
 
 /////this below is for the side menu
 (function (window, document) {
@@ -156,87 +161,6 @@ $(".genre").change($(".genre").val());
   document.addEventListener("click", handleEvent);
 })(this, this.document);
 
-// ////// add to wish list
-
-// var items = $("#items");
-
-// var wishes = [];
-
-// // The following function renders items in a todo list as <li> elements
-// function renderWishes() {
-//   // Clear todoList element and update todoCountSpan
-//   $("#wishList").html("")
-//   $("#gameCount").text(wishes.length);
-
-//   // Render a new li for each todo
-//   for (var i = 0; i < wishes.length; i++) {
-//     var wish = wishes[i];
-
-//     var li = $("<li></li>").text(wish);
-
-//     li.setAttribute("data-index", i);
-
-//     var button = document.createElement("button");
-//     button.textContent = "Complete ✔️";
-
-//     li.appendChild(button);
-//     todoList.appendChild(li);
-//   }
-// }
-
-// function init() {
-//   // Get stored games from localStorage
-//   var storedWishes = JSON.parse(localStorage.getItem("wish"));
-
-//   // If todos were retrieved from localStorage, update the todos array to it
-//   if ($("#wish") !== null) {
-//     wish = $("#wish");
-//   }
-
-//   renderWishes();
-// }
-
-// function storeWishes() {
-//   // Stringify and set key in localStorage to todos array
-//   localStorage.setItem("wish", JSON.stringify(wish));
-// }
-
-// // Add submit event to form
-
-// $("wish").submit(function (e) {
-//   e.preventDefault();
-
-//   var wishText = wish.value().trim();
-
-//   // Return from function early if submitted todoText is blank
-//   if (wishText === "") {
-//     return;
-//   }
-
-//   // Add new todoText to todos array, clear the input
-//   wish.push(wishText);
-//   wish.val("");
-
-//   // Store updated todos in localStorage, re-render the list
-//   storeWishes();
-//   renderWishes();
-// });
-
-// // Add click event to todoList element
-// todoList.addEventListener("click", function (event) {
-//   var element = event.target;
-
-//   // Checks if element is a button
-//   if (element.matches("button") === true) {
-//     // Get its data-index value and remove the todo element from the list
-//     var index = element.parentElement.getAttribute("data-index");
-//     todos.splice(index, 1);
-
-//     // Store updated todos in localStorage, re-render the list
-//     storeTodos();
-//     renderTodos();
-//   }
-// });
-
-// // Calls init to retrieve data and render it to the page on load
-// init();
+$("#genre").on("change", function (event) {
+  myFetch(event.target.value);
+});
